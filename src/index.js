0789862e777447b337d922e851092ce7b7f7a87a
StanPri <spribytkovsky@yahoo.com>
2017-03-06 12:58:48 -0800

    Production build setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*eslint-disable import/default */
 import 'babel-polyfill';
 import 'whatwg-fetch';
 import React from 'react';
---
+++ src/store/configureStore.prod.js
@@ -0,0 +1,16 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import rootReducer from '../reducers';
+import thunk from 'redux-thunk';
+import api from '../middleware/api';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function configureStore(initialState) {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(
+      applyMiddleware(thunk, api)
+    )
+  );
+}
